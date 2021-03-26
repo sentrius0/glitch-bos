c261cbeb10bf95990ae71a7ff7603f3e7247f0a9
Sentrius <80965721+sentrius0@users.noreply.github.com>
2021-03-26 17:55:07 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+
+<a href="https://glitch.com/edit/?utm_content=project_discord-js-v12-bosaltyapi&utm_source=remix_this&utm_medium=button&utm_campaign=glitchButton#!/remix/discord-js-v12-bosaltyapi">
+  <img src="https://cdn.glitch.com/2bdfb3f8-05ef-4035-a06e-2043962a3a13%2Fremix%402x.png?1513093958726" alt="remix this" height="33">
+</a>
+
+| Bu Dosya discord.js v12 ye göre düzenlenmiştir. |
+|--|--|
+|  |
+
+ayarlar.json'u doldurursanız bot aktif olacaktır
+------------------------------------------------
+komut eklerken **KOMUTLAR KISMINA ADD KISMINA BASARAK** yeni dosya oluşturun
+-------------------------------------------------
+Çoğu kişinin kullandığı klasik sade altyapıdır!
+----------
+
+
---
+++ events/ready.js
@@ -0,0 +1,46 @@
+const chalk = require("chalk");
+const moment = require("moment");
+const Discord = require("discord.js");
+const ayarlar = require("../ayarlar.json");
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+  console.log(
+    ` [${moment().format(
+      "YYYY-MM-DD HH:mm:ss"
+    )}] BOT: Aktif, Komutlar yüklendi!`
+  );
+  console.log(
+    ` [${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: ${
+      client.user.username
+    } ismi ile giriş yapıldı!`
+  );
+  client.user.setStatus("dnd");
+/*  
+  idle = boşta
+  dnd = rahatsız etmeyin
+  online = çevrimiçi
+  */
+  client.user.setActivity("Sentrius boş altyapı", {
+    type: "LISTENING"
+   /* 
+    LISTENING = DİNLİYOR
+    WATCHING = İZLİYOR
+    PLAYING = OYNUYOR
+    STREAMING = YAYINDA
+    */
+  });
+  console.log(
+    ` [${moment().format("YYYY-MM-DD HH:mm:ss")}] Oyun ismi ayarlandı!`
+  );
+  console.log(
+    ` [${moment().format("YYYY-MM-DD HH:mm:ss")}] BOT: Şu an ` +
+      client.channels.cache.size +
+      ` adet kanala, ` +
+      client.guilds.cache.size +
+      ` adet sunucuya ve ` +
+      client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() +
+      ` kullanıcı!`
+  );
+};
